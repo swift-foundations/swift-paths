18f4b1c1fc897ecd1f4b91c3e3907ec255bcd093
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-25 15:00:23 +0100

    Flatten test structure and migrate to Apple Swift Testing

--- Package.swift
+++ Package.swift
@@ -25,7 +25,13 @@ let package = Package(
                 .product(name: "Kernel Primitives", package: "swift-kernel-primitives"),
                 .product(name: "Binary Primitives", package: "swift-binary-primitives")
             ]
-        )
+        ),
+        .testTarget(
+            name: "Paths Tests",
+            dependencies: [
+                "Paths",
+            ]
+        ),
     ]
 )
 
@@ -35,6 +41,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("ExistentialAny"),
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
         .enableExperimentalFeature("Lifetimes"),
         .enableExperimentalFeature("SuppressedAssociatedTypes"),
         .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
